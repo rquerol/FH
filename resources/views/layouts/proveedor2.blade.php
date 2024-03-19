e2ba78f515a58c8a0876d44c628a3a596d775e1c
poolgp <145465364+poolgp@users.noreply.github.com>
2024-03-19 17:52:58 +0100

    proveedor 19/03/2024

--- resources/views/layouts/proveedor2.blade.php
+++ resources/views/layouts/proveedor2.blade.php
@@ -10,11 +10,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="{{ asset('css/proveedor2.css') }}" />
 </head>
 
 <body>
     @yield('contenido')
+
+    <div class="contenedor">
+        <div class="infoProveedor"></div>
+        <div class="estadisticas"></div>
+        <div class="crearMenu"></div>
+        <div class="entregarMenu"></div>
+        <div class="stok"></div>
+        <div class="mapa"></div>
+    </div>
+
     <h1>Pol Garcia</h1>
     <p>esta es la segunda pagina del proveedor, no la del mapa</p>
 
