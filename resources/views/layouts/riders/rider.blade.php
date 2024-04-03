162469944276332d3c83be7cfbc2d6c7abec8cde
Moheto22 <mohatae61@gmail.com>
2024-04-03 18:39:41 +0200

    menu nav-bar

--- resources/views/layouts/riders/rider.blade.php
+++ resources/views/layouts/riders/rider.blade.php
@@ -15,7 +15,158 @@
     <div id="map">
         <img src="{{ asset('img/superhero.png') }}" alt="" id="super">
         <button id="createMarkerButton">Crear Marca</button>
+    
+    
+          <!-- Navbar Inferior -->
+    <nav class="navbar-bottom">
+        <div class="container text-center">
+            <div class="navbar-item">
+                <a href="#">
+                    <img src="{{ asset('img/perfil.png') }}" alt="Perfil" class="img-fluid" />
+                    <div>Perfil</div>
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">
+                    <img src="{{ asset('img/crear_pua.png') }}" alt="Crear Pua" class="img-fluid" />
+                    <div>Crear Pua</div>
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="#" data-toggle="modal" data-target="#modal-reservas">
+                    <img src="{{ asset('img/reservas.png') }}"  alt="Reservar" class="img-fluid" />
+                    <div>Reservar</div>
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="#" data-toggle="modal" data-target="#modal-historial">
+                    <img src="{{ asset('img/historial.png') }}" alt="Historial" class="img-fluid" />
+                    <div>Historial</div>
+                </a>
+            </div>
+        </div>
+    </nav>
+        
+    <!-- Modal Reservas -->
+    <div id="modal-reservas" class="modal fade">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">RESERVAS</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>PROVEEDOR</th>
+                                    <th>RESERVAS</th>
+                                    <th>HORARIOS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                        
+                                <tr>
+                                    <td>Pizzeria Rebebba</td>
+                                    <td>4</td>
+                                    <td>18:00 a 22:00</td>
+                                </tr>
+                                <tr>
+                                    <td>BurgerKing</td>
+                                    <td>2</td>
+                                    <td>20:00 a 20:30</td>
+                                </tr>
+                                <tr>
+                                    <td>365</td>
+                                    <td>2</td>
+                                    <td>12:00 a 24:00</td>
+                                </tr>
+                            
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- Modal Historial -->
+    <div id="modal-historial" class="modal fade">
+        <div class="modal-dialog modal-lg mt-3">
+            <div class="modal-content px-5 pt-3 pb-3">
+                <div class="row">
+                    <div class="col-lg-12 contact-form">
+                        <div class="card border-0">
+                            <div class="card-body">
+                                <div class="text-end">
+                                    <!-- Cambiar btn-close a close -->
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="card-title text-center pb-3">
+                                    <h3>HISTORIAL</h3>
+                                    <p class="lead text-muted fw-light">Descubre el historial de tus puas.</p>
+                                </div>
+                                <div class="row mb-4">
+                                    <div class="col-md-6">
+                                        <div class="text-center">
+                                            <h4>PUAS CREADAS</h4>
+                                            <div class="d-flex justify-content-center align-items-center">
+                                                <p class="lead m-0">12</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="text-center">
+                                            <h4>MENÚS ENTREGADOS</h4>
+                                            <div class="d-flex justify-content-center align-items-center">
+                                                <p class="lead m-0">5</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-4">
+                                    <div class="col-md-6">
+                                        <div class="text-center">
+                                            <h4>RESERVAS ACTUALES</h4>
+                                            <div class="d-flex justify-content-center align-items-center">
+                                                <p class="lead m-0">3</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="text-center">
+                                            <h4>RANKING</h4>
+                                            <div class="d-flex justify-content-center align-items-center">
+                                                <p class="lead m-0">5</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     </div> 
+
+
+
+
+
     <!-- Enlace al archivo JavaScript -->
     <script src="{{ asset('js/rider.js') }}"></script>
 </body>
