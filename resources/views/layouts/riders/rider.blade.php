08b877f7c18e4cafee1ec38866273e1f05806ab5
frangaar <dnerizb2223@politecnics.barcelona>
2024-04-04 16:57:53 +0200

    Nav casi acabado

--- resources/views/layouts/riders/rider.blade.php
+++ resources/views/layouts/riders/rider.blade.php
@@ -14,8 +14,6 @@
     @yield('contenido')
     <div id="map">
         <img src="{{ asset('img/superhero.png') }}" alt="" id="super">
-        <button id="createMarkerButton">Crear Marca</button>
-        <!-- The Modal -->
         <div id="myModal" class="modal-pua">
             <div class="modal-content-pua">
                 <span class="close" id="closeButton">&times;</span>
@@ -36,41 +34,35 @@
         <nav class="navbar-bottom">
             <div class="container text-center d-flex ">
                 <div class="navbar-item">
-                    <a href="#">
+                    <button id="boton-perfil">
                         <img src="{{ asset('img/perfil.png') }}" alt="Perfil" class="img-fluid" />
-                        <div>Perfil</div>
-                    </a>
+                    </button>
                 </div>
                 <div class="navbar-item">
-                    <a href="#">
+                    <button id="createMarkerButton">
                         <img src="{{ asset('img/crear_pua.png') }}"alt="Crear Pua" class="img-fluid" />
-                        <div>Crear Pua</div>
-                    </a>
+                    </button>
                 </div>
                 <div class="navbar-item">
-                    <a href="#" data-toggle="modal" data-target="#modal-reservas">
+                    <button id="boton-reservas">
                         <img src="{{ asset('img/reservas.png') }}" alt="Reservar" class="img-fluid" />
-                        <div>Reservar</div>
-                    </a>
+                    </button>
                 </div>
                 <div class="navbar-item">
-                    <a href="#" data-toggle="modal" data-target="#modal-historial">
-                    <img src="{{ asset('img/historial.png') }}"alt="Historial" class="img-fluid" />
-                        <div>Historial</div>
-                    </a>
+                    <button id="boton-historial">
+                        <img src="{{ asset('img/historial.png') }}"alt="Historial" class="img-fluid" />
+                    </button>
                 </div>
             </div>
         </nav>
       
             <!-- Modal Reservas -->
-        {{-- <div id="modal-reservas" class="modal fade">
-            <div class="modal-dialog modal-lg">
+        <div id="modal-reservas" class="modal-reservas">
+            <div class="modal-content-reservas modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">RESERVAS</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                        <span class="close" id="closeButtonReservas">&times;</span>
                     </div>
                     <div class="modal-body">
                         <div class="table-responsive">
@@ -105,25 +97,20 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div> 
 
-            <!-- Modal Historial -->
-        {{-- <div id="modal-historial" class="modal fade">
-            <div class="modal-dialog modal-lg mt-3">
+        <div class="modal-historial" id="modal-historial">
+            <div class="modal-content-historial modal-lg mt-3">
                 <div class="modal-content px-5 pt-3 pb-3">
                     <div class="row">
                         <div class="col-lg-12 contact-form">
                             <div class="card border-0">
                                 <div class="card-body">
                                     <div class="text-end">
-                                        <!-- Cambiar btn-close a close -->
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
+                                    <span class="close" id="closeButtonHistorial">&times;</span>
                                     </div>
                                     <div class="card-title text-center pb-3">
                                         <h3>HISTORIAL</h3>
@@ -165,20 +152,13 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="text-center">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div> --}}
-
-
-
-
+        </div> 
     </div> 
     <!-- Enlace al archivo JavaScript -->
     <script src="{{ asset('js/rider.js') }}"></script>
