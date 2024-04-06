ff85c678365553f38b696d8213306cb340e47464
poolgp <145465364+poolgp@users.noreply.github.com>
2024-04-06 12:50:34 +0200

    06/04/2024

--- resources/views/layouts/proveedor/proveedor2.blade.php
+++ resources/views/layouts/proveedor/proveedor2.blade.php
@@ -7,13 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proveedor2</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src='https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.css' rel='stylesheet' />
 
     <link rel="stylesheet" href="{{ asset('css/proveedor2.css') }}">
 </head>
 
-{{-- <body>
+<body>
     <div class="contenedor col-lg-12">
         <div class="fila1 col-lg-12">
             <div class="infoProveedor col-lg-7 col-sm-12">
@@ -32,7 +33,24 @@
 
         <div class="fila2 col-lg-12">
             <div class="crearMenu col-lg-4 col-sm-12">
-                <h2>Crear Menu</h2>
+                <div class="tituloMenu">
+                    <h2>Crear Menu</h2>
+                </div>
+                <div class="cantidadMenu">
+                    <div class="menosMenu">
+                        <button class="menos"> - </button>
+                    </div>
+                    <div class="cantMenu"> 3 </div>
+                    <div class="masMenu">
+                        <button class="mas"> + </button>
+                    </div>
+                </div>
+                <div class="btnCrear">
+                    addcslashes
+                        <!-- <button class="crear">
+                            Crear Menu(s)
+                        </button> -->
+                </div>
             </div>
             <div class="entregarMenu col-lg-7 col-sm-12">
                 <h2>Entregar Menu</h2>
@@ -43,11 +61,14 @@
             <div class="stok col-lg-5 col-sm-12">
                 <h2>Stok</h2>
             </div>
-            <div class="mapa col-lg-5 col-sm-12">
-                <h2>Mapa</h2>
-                <a href="{{ route('proveedor1') }}">
-                    <button class="volverMapa">volver mapa</button>
-                </a>
+            <div class="mapa col-lg-5">
+                @yield('contenido')
+                <div id="map">
+                    <a href="{{ route('proveedor2') }}">
+                        <button class="mover" id="mover">Gestionar Proveedor</button>
+                    </a>
+                </div>
+                <script src="{{ asset('js/rider.js') }}"></script>
             </div>
         </div>
     </div>
@@ -55,57 +76,9 @@
     <div class="container-fluid">
         @yield('contenido')
     </div>
-</body> --}}
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="contenedor col-lg-12">
-                <div class="row">
-                    <div class="infoProveedor col-lg-7 col-sm-12">
-                        <h2>Proveedor</h2>
-                        <h5>Calle: </h5>
-                        <p>C/ Mallorca 283</p>
-                        <h5>Ciudad: </h5>
-                        <p>Barcelona</p>
-                        <h5>CP: </h5>
-                        <p>08037</p>
-                    </div>
-                    <div class="estadisticas col-lg-4 col-sm-12">
-                        <h2>Estadisticas</h2>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="crearMenu col-lg-4 col-sm-12">
-                        <h2>Crear Menu</h2>
-                    </div>
-                    <div class="entregarMenu col-lg-7 col-sm-12">
-                        <h2>Entregar Menu</h2>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="stok col-lg-5 col-sm-12">
-                        <h2>Stok</h2>
-                    </div>
-                    <div class="mapa col-lg-5 col-sm-12">
-                        <h2>Mapa</h2>
-                        <a href="{{ route('proveedor1') }}">
-                            <button class="volverMapa">volver mapa</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="container-fluid">
-            @yield('contenido')
-        </div>
 </body>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
 
 </html>
