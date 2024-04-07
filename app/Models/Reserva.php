e872cf314d004cf914e6281015c88f8659b55f52
oscarArriaza <arriaza22598@vk.com>
2024-04-07 04:31:33 +0200

    32

---
+++ app/Models/Reserva.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reserva extends Model
+{
+    use HasFactory;
+    protected $table="reservas";
+    //protected $primaryKey="nombreDeLaClavePrimaria"; //solo se pone cuando la clave primaria no se llama id.
+    //public $incrementing=false; //solo se pone cuando la clave primaria no es autoincremental.
+    //protected $keyType="string"; //solo se pone cuando la clave primaria no es entero.
+    public $timestamps=false;
+}
