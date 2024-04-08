5b9efa064e78452481088cad80f02676ce640526
oscarArriaza <arriaza22598@vk.com>
2024-04-08 11:11:10 +0200

    34

---
+++ app/Models/Pua.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pua extends Model
+{
+    use HasFactory;
+    protected $table="puas";
+    //protected $primaryKey="nombreDeLaClavePrimaria"; //solo se pone cuando la clave primaria no se llama id.
+    //public $incrementing=false; //solo se pone cuando la clave primaria no es autoincremental.
+    //protected $keyType="string"; //solo se pone cuando la clave primaria no es entero.
+    public $timestamps=false;
+}
