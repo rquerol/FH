cf7d4bddefecdb8ff07ec97f8f0d68fb3f6ac0f1
oscarArriaza <arriaza22598@vk.com>
2024-04-08 20:22:42 +0200

    35

--- resources/views/partials/mensajes.blade.php
+++ resources/views/partials/mensajes.blade.php
@@ -1,17 +1,17 @@
 @if (Session::has("error"))
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    {{Session::get("error")}}
-    <button type="button" class="close" data-dissmiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{Session::get("error")}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
 @endif
 
 @if (Session::has("mensaje"))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{Session::get("mensaje")}}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
- @endif
+
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{Session::get("mensaje")}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    
+@endif
